c4dc3ed54548610571ec20a10155019241c18375
luejerry <luejerry@gmail.com>
2017-06-15 00:52:37 -0500

    Fix prev/next arrow positions to screen

--- htmlgenerator.py
+++ htmlgenerator.py
@@ -13,20 +13,22 @@ htmltemplatehead = ['<!doctype html>',
                     'body {font-size: 80px; background-color: #F0F0F0}',
                     # 'div {display: inline-block}',
                     'img {box-shadow: 0px 0px 10px 0px #8A8A8A; border-radius: 3px}',
-                    '.next, .prev {position: absolute; top: 0; bottom: 0; display: inline-flex; flex-direction: column; justify-content: center;}',
+                    '.next, .prev {position: absolute; top: 0; bottom: 0; display: inline-flex; flex-direction: column; justify-content: center; z-index: 10}',
                     '.prev {left: 0; right: 50%; text-align:left; align-items: flex-start}',
                     '.next {left: 50%; right: 0; text-align:right; align-items: flex-end}',
                     'a {opacity: 0; color: #888888; transition: opacity 0.2s}',
-                    'a:hover {opacity: 1;}'
+                    'a:hover {opacity: 1;}',
+                    '.arrow {position: fixed; top: 50%; z-index: -1}',
                     '</style>',
                     '</head>',
                     '<body>']
 htmltemplatefoot = ['</body></html>']
 htmltemplateimg = '<div id="{id}" align="center" style="position:relative">\n' \
                   '<a href="#{previd}">' \
-                  '<span class="prev">❮</span></a>\n' \
+                  '<span class="prev">' \
+    '<div class="arrow">❮</div></span></a>\n' \
                   '<a href="#{nextid}">' \
-                  '<span class="next">❯</span></a>\n' \
+                  '<span class="next"><div class="arrow">❯</div></span></a>\n' \
                   '<img src={img} /></div>\n'
 
 def render(path):
