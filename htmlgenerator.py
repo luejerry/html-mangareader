167f8179db3d6ee20d80aa40520b48d2eda18905
Jerry Lue <luejerry@gmail.com>
2017-06-15 16:28:47 -0500

    Add function to extract only images to zip archives

--- htmlgenerator.py
+++ htmlgenerator.py
@@ -3,11 +3,13 @@ import os
 import tempfile
 from string import Template
 import pathlib
+import zipfile
+
 import templates
 
 def render_from_template(path, 
                          doc_template, page_template, img_types,
-                         outfile=os.path.join(tempfile.gettempdir(), 'render.html')):
+                         outfile=os.path.join(tempfile.gettempdir(), 'html-mangareader', 'render.html')):
     files = filter(lambda e: os.path.isfile(os.path.join(path, e)),
                    os.listdir(path))
     imagefiles = filter(lambda f: f.split('.')[-1].lower() in img_types, files)
@@ -15,6 +17,7 @@ def render_from_template(path,
         print('No image files were found in path: {0}'.format(path))
         return
     imagepaths = [pathlib.Path(os.path.abspath(p)).as_uri() for p in imagefiles]
+    os.makedirs(os.path.dirname(outfile), exist_ok=True)
     with open(outfile, 'w', encoding='utf-8', newline='\r\n') as renderfd:
         doc_template = Template(templates.DOC_TEMPLATE)
         img_template = Template(templates.IMG_TEMPLATE)
@@ -25,7 +28,17 @@ def render_from_template(path,
     print("view saved to " + outfile)
     return
 
+def extract_zip(path, img_types, outpath=os.path.join(tempfile.gettempdir(), 'html-mangareader')):
+    with zipfile.ZipFile(path, mode='r') as zip_file:
+        imagefiles = filter(lambda f: f.split('.')[-1].lower() in img_types, zip_file.namelist())
+        if not imagefiles:
+            print('No image files were found in archive: {}'.format(path))
+            return None
+        return zip_file.extractall(outpath, imagefiles)
+
 if __name__ == '__main__':
     path = '.' if len(sys.argv) <= 1 else sys.argv[1]
     # render_from_template(path, templates.DOC_TEMPLATE, templates.IMG_TEMPLATE, templates.DEFAULT_IMAGETYPES, os.path.join(path, 'render.html'))
     render_from_template(path, templates.DOC_TEMPLATE, templates.IMG_TEMPLATE, templates.DEFAULT_IMAGETYPES)
+    # outpath = extract_zip('testar.zip', templates.DEFAULT_IMAGETYPES)
+    # print(outpath)
