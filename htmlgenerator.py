9d84298474590762a4e3dee2affc4a3e4de3ec34
luejerry <luejerry@gmail.com>
2017-06-16 00:07:02 -0500

    Images from zip archives are now referenced explicitly, instead of referencing the extraction directory

--- htmlgenerator.py
+++ htmlgenerator.py
@@ -14,15 +14,10 @@ from excepts import ImagesNotFound
 
 
 # todo: take list of explicit image paths, which are passed in from listdir or the unzip
-def render_from_template(path,
+def render_from_template(paths,
                          doc_template, page_template, img_types,
                          outfile=os.path.join(tempfile.gettempdir(), 'html-mangareader', 'render.html')):
-    files = filter(lambda e: os.path.isfile(os.path.join(path, e)),
-                   os.listdir(path))
-    imagefiles = filter(lambda f: f.split('.')[-1].lower() in img_types, files)
-    if not imagefiles:
-        raise ImagesNotFound('No image files were found in path: {0}'.format(path))
-    imagepaths = [pathlib.Path(os.path.join(path, p)).as_uri() for p in sorted(imagefiles, key=filename_comparator)]
+    imagepaths = [pathlib.Path(p).as_uri() for p in paths]
     os.makedirs(os.path.dirname(outfile), exist_ok=True)
     with open(outfile, 'w', encoding='utf-8', newline='\r\n') as renderfd:
         html_template = Template(doc_template)
@@ -34,23 +29,31 @@ def render_from_template(path,
     print("view saved to " + outfile)
     return outfile
 
+def scan_directory(path, img_types):
+    files = filter(lambda f: os.path.isfile(os.path.join(path, f)), os.listdir(path))
+    imagefiles = filter(lambda f: f.split('.')[-1].lower() in img_types, files)
+    return [os.path.join(path, p) for p in sorted(imagefiles, key=filename_comparator)]
 
 def extract_zip(path, img_types, outpath=os.path.join(tempfile.gettempdir(), 'html-mangareader')):
     with zipfile.ZipFile(path, mode='r') as zip_file:
-        imagefiles = filter(lambda f: f.split('.')[-1].lower() in img_types, zip_file.namelist())
+        imagefiles = list(filter(lambda f: f.split('.')[-1].lower() in img_types, zip_file.namelist()))
         if not imagefiles:
             raise ImagesNotFound('No image files were found in archive: {}'.format(path))
         zip_file.extractall(outpath, imagefiles)
-        return outpath
+        return [os.path.join(outpath, image) for image in sorted(imagefiles, key=filename_comparator)]
 
 
 def extract_render(path, doc_template, page_template, img_types, outpath):
-    imgpath = path
     if os.path.isfile(path):
         try:
             imgpath = extract_zip(path, img_types)
         except zipfile.BadZipFile:
             print('{} does not appear to be a valid zip/cbz file.'.format(path))
+            return
+    else:
+        imgpath = scan_directory(path, img_types)
+        if not imgpath:
+            print('No images were found in {}'.format(path))
             return
     try:
         renderfile = render_from_template(imgpath, doc_template, page_template, img_types)
