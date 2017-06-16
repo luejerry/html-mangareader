8c8551cf5ec6ebeb410dc47bd4b5da2a3e2c2cb4
luejerry <luejerry@gmail.com>
2017-06-15 19:59:36 -0500

    Fix IE/Edge bug with arrows not hiding

--- templates.py
+++ templates.py
@@ -5,12 +5,13 @@ DOC_TEMPLATE = (
     '<style>'
     'body {font-size: 80px; background-color: #F0F0F0}'
     'img {box-shadow: 0px 0px 10px 0px #8A8A8A; border-radius: 3px}'
-    '.next, .prev {position: absolute; top: 0; bottom: 0; display: inline-flex; flex-direction: column; justify-content: center; z-index: 10}'
+    '.next, .prev {position: absolute; top: 0; bottom: 0; display: inline-flex; flex-direction: column; justify-content: center; z-index: 10; opacity: 0; transition: opacity 0.2s}'
+    '.next:hover, .prev:hover {opacity: 1}'
     '.prev {left: 0; right: 50%; text-align:left; align-items: flex-start}'
     '.next {left: 50%; right: 0; text-align:right; align-items: flex-end}'
-    'a {opacity: 0; color: #888888; transition: opacity 0.2s}'
-    'a:hover {opacity: 1;}'
-    '.arrow {position: fixed; top: 50%; margin-top: -50px; z-index: -1}'
+    # 'a {opacity: 0; color: #888888; transition: opacity 0.2s}'
+    # 'a:hover {opacity: 1;}'
+    '.arrow {position: fixed; top: 50%; margin-top: -50px; z-index: -1; color: #888888}'
     '</style>'
     '</head>'
     '<body>'
@@ -20,7 +21,7 @@ DOC_TEMPLATE = (
 )
 
 IMG_TEMPLATE = (
-    '<div id="$id" align="center" style="position:relative">\n'
+    '<div id="$id" align="center" style="position:relative; margin: 50px 0">\n'
     '<a href="#$previd">'
     '<span class="prev">'
     '<div class="arrow">❮</div>'
