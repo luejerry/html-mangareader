ea5ce0396e2ba869b762ba6960255095dff556bf
luejerry <luejerry@gmail.com>
2017-06-17 05:25:16 -0500

    Fix error box erroneously appearing when pyinstaller builds with -window option

--- mangareader.py
+++ mangareader.py
@@ -3,18 +3,24 @@ from tkinter import Tk, messagebox
 from mangarender import extract_render
 import templates
 
-if __name__ == '__main__':
+
+def main():
     if len(sys.argv) <= 1:
         Tk().withdraw()
         messagebox.showinfo('HTML MangaReader - simply the fastest comic book reader',
-                            'Start reading your favorite comics by doing one of the following:\n'
+                            'Read your favorite comics by doing one of the following:',
+                            detail=
                             '- Drag an image folder or file onto the MangaReader icon, or\n'
                             '- Drag a ZIP or CBZ archive onto the MangaReader icon')
-        exit(0)
+        return
     path = '.' if len(sys.argv) <= 1 else sys.argv[1]
     try:
-        extract_render(path, templates.DOC_TEMPLATE, templates.IMG_TEMPLATE, templates.BOOT_TEMPLATE, templates.DEFAULT_IMAGETYPES)
+        extract_render(path, templates.DOC_TEMPLATE, templates.IMG_TEMPLATE, templates.BOOT_TEMPLATE,
+                       templates.DEFAULT_IMAGETYPES)
     except Exception as e:
         Tk().withdraw()
         messagebox.showerror('MangaReader encountered an error: ' + type(e).__name__,
                              str(e))
+
+if __name__ == '__main__':
+    main()
